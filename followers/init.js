6c24c12fd88908054c50d3d0d93bd25ea13739a3
Aoyama Nanami <aoyama_nanami@sakurasou>
2015-02-05 17:52:09 +0800

    use worker for computation

--- followers/init.js
+++ followers/init.js
@@ -54,211 +54,63 @@ function Compute(missions, required_ilv, max_ilv) {
     container.empty()
 
     for (var i = 0; i < missions.length; i++) {
+        missions[i].score = 90 * missions[i].ability.length + 30 * missions[i].slot
+        delete missions[i].element
+    }
+
+    var worker = new Worker("worker.js")
+    worker.postMessage({"missions": missions, "followers": my_followers,
+        "required_ilv": required_ilv, "max_ilv": max_ilv,
+        "garrison_abilities": g_garrison_abilities, "garrison_followers": g_garrison_followers,})
+
+    for (var i = 0; i < missions.length; i++) {
         var a = $("<a>").attr("href", "http://ptr.wowhead.com/mission=" + missions[i].id)
             .addClass("mission_link")
         var table = $("<table>")
         missions[i].element = table
-        missions[i].score = 90 * missions[i].ability.length + 30 * missions[i].slot
-        
         container.append([a, table, "<hr/>"])
     }
 
-    var candidates = missions.map(MasterPlan)
-    var indexes = missions.map(function(){ return 0 })
-    var best_list = []
-    var best = 9999999
-
-    function NextPermutation(a, c) {
-        for (var i = 0; i < a.length; i++) {
-            a[i]++
-            if (a[i] == c[i].length) {
-                a[i] = 0
-            } else {
-                return true
-            }
-        }
-        return false
-    }
-
-    do {
-        var rows = indexes.map(function(x, i){ return candidates[i][x] })
-        var result = CombineParty(rows, my_followers, required_ilv, max_ilv, missions)
-        var party = result[0]
-        var score = result[1]
-        if (score < best) {
-            best = score
-            best_list = []
-        }
-        if (score <= best) {
-            best_list.push({"party": party, "rows": rows})
-        }
-    } while (NextPermutation(indexes, candidates))
-
     var output_tables = missions.map(function(m){ return m.element })
 
-    best_list.forEach(function(p, i) {
-        var tr = $("<tr>")
-        Object.keys(p.party).forEach(function(x, j) {
-            var td = $("<td>")
-            td.append(CreateFollowerTooltip(my_followers[x]))
-            td.append($("<span>").html("(" + my_followers[x].iLevel + " &#8594; " + p.party[x] + ")"))
-            tr.append(td)
-        })
-        tr.append($("<td>").text("+" + best + " iLevel"))
+    worker.onmessage = function(e) {
+        var best_list = e.data.best_list
+        var best = e.data.best_score
 
-        p.elements = []
-        for (var j = 0; j < p.rows.length; j++) {
-            if (typeof p.rows[j].element == "undefined") {
-                p.rows[j].element = CreateFollowerPartyRow(p.rows[j], missions[i].score)
-                $(output_tables[j]).append(p.rows[j].element)
-            }
-            p.elements.push(p.rows[j].element)
-        }
-
-
-        $(tr).hover(
-            function(){ $.each(p.elements, function(i, e) { e.addClass("highlight") }) },
-            function(){ $.each(p.elements, function(i, e) { e.removeClass("highlight") }) }
-        )
-
-        $("#minimal_party").append(tr)
-    })
-
-    $("#message").text("computation time: " + (new Date().getTime() - start_time) + "ms")
-}
-
-function CombineParty(p, followers, required_ilv, max_ilv, missions) {
-    var o = {}
-
-    p.forEach(function(x, i) {
-        var target_ilv = required_ilv + Math.ceil((missions[i].score - x.score) / 3)
-        target_ilv = Math.min(target_ilv, max_ilv)
-        x.party.forEach(function(y) {
-            o[y] = Math.max(o[y] || 0, target_ilv)
-        })
-    })
-
-    var ilv_diff = 0
-    Object.keys(o).forEach(function(x){
-        ilv_diff += o[x] - followers[x].iLevel
-    })
-
-    return [o, ilv_diff]
-}
-
-
-
-function MasterPlan(mission) {
-    var result = []
-
-    for (var i = 0; i < my_followers.length; i++) {
-        for (var j = i + 1; j < my_followers.length; j++) {
-            for (var k = j + 1; k < my_followers.length; k++) {
-                var score = MissionSuccessRate(
-                    mission,
-                    [my_followers[i], my_followers[j], my_followers[k]],
-                    mission.score
-                )
-                result.push({"score": score, "party": [i, j, k]})
-            }
-        }
-    }
-
-    result.sort(function(x, y) { return y.score - x.score })
-    if (result.length > 0) {
-        var best_score = result[0].score
-        result = result.filter(function(r) { return r.score >= best_score - 30 })
-    }
-
-    return result
-}
-
-function log_debug(message) {
-}
-
-function MissionSuccessRate(mission, followers, mission_score) {
-    var my_score = 30 * followers.length
-    var mission_type_countered = false
-    var racial_bonus = 0
-    var counter_bonus = 0
-
-    var traits = {}
-    var epic_mount = 0
-    var high_stamina = 0
-    var burst_of_power = 0
-    var combat_experience = 0
-    var dancer = 0
-
-    var ability_map = {}
-    mission.ability.forEach(function(ability) {
-        ability_map[ability] = (ability_map[ability] || 0) + 1
-    });
-
-    followers.forEach(function(follower) {
-        log_debug("follower id: " + follower.garrFollowerID)
-
-        var is_dancer = false
-        follower.ability.forEach(function(ability) {
-            var detail = g_garrison_abilities[ability.id]
-            if (detail.trait) {
-                traits[ability.id] = traits[ability.id] || 0
-                traits[ability.id] += 1
-
-                if (ability.id == 232) {  // dancer
-                    is_dancer = true
-                } else if (detail.category == 1 || detail.category == 6) {
-                    // slayer / environment preference
-                    if (detail.counters.indexOf(mission.type) >= 0) {
-                        log_debug(detail.name)
-                        mission_type_countered = true;
-                    }
-                } else if (detail.category == 4) {  // racial preference
-                    if (MatchRace(detail.race, followers, follower.garrFollowerID)) {
-                        log_debug(detail.name)
-                        racial_bonus += 1
-                    }
+        best_list.forEach(function(p, i) {
+            var tr = $("<tr>")
+            Object.keys(p.party).forEach(function(x, j) {
+                var td = $("<td>")
+                td.append(CreateFollowerTooltip(my_followers[x]))
+                if (my_followers[x].iLevel != p.party[x]) {
+                    td.append($("<span>").html("(" + my_followers[x].iLevel + " &#8594; " + p.party[x] + ")"))
+                } else {
+                    td.append($("<span>").text("(" + p.party[x] + ")"))
                 }
-            } else {
-                detail.counters.forEach(function(counter) {
-                    var a = ability_map[counter]
-                    if (a) {
-                        ability_map[counter] -= 1
-                        counter_bonus += 1
-                    }
-                })
+                tr.append(td)
+            })
+            tr.append($("<td>").text("+" + best + " iLevel"))
+
+            p.elements = []
+            for (var j = 0; j < p.rows.length; j++) {
+                if (typeof p.rows[j].element == "undefined") {
+                    p.rows[j].element = CreateFollowerPartyRow(p.rows[j], missions[j].score)
+                    $(output_tables[j]).append(p.rows[j].element)
+                }
+                p.elements.push(p.rows[j].element)
             }
+
+            $(tr).hover(
+                function(){ $.each(p.elements, function(i, e) { e.addClass("highlight") }) },
+                function(){ $.each(p.elements, function(i, e) { e.removeClass("highlight") }) }
+            )
+
+            $("#minimal_party").append(tr)
         })
 
-        /*
-        if (is_dancer && follower.ability.indexOf(6) >= 0) {
-            dancer += 1
-        }
-        */
-    })
+        $("#message").text("computation time: " + (new Date().getTime() - start_time) + "ms")
 
-    var time = 8 / Math.pow(2, (traits[221] || 0))
-    if (time >= 7) {  // high_stamina
-        my_score += (traits[76] || 0) * 30
-    } else {  // Burst
-        my_score += (traits[77] || 0) * 30
+        $WowheadPower.refreshLinks()
     }
-    my_score += (traits[201] || 0) * 30
 
-    if (mission_type_countered) my_score += 30
-    my_score += racial_bonus * 45
-    my_score += counter_bonus * 90
-    my_score += dancer * 45
-
-    log_debug("my score: " + my_score)
-
-    if (my_score > mission_score) my_score = mission_score
-
-    return my_score
-}
-
-function MatchRace(race, followers, my_id) {
-    return followers.some(function(follower) {
-        if (follower.garrFollowerID == my_id) return false
-        return race.indexOf(g_garrison_followers[follower.garrFollowerID].horde.race) >= 0
-    })
 }
